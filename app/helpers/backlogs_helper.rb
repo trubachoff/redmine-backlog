e93c731d85bf04f289f3e28163d1a553342d3f3d
Vladimir Trubachoff <trubachoff@gmail.com>
2016-09-22 19:21:11 +0300

    Fixed description and clear backlog helper

--- app/helpers/backlogs_helper.rb
+++ app/helpers/backlogs_helper.rb
@@ -1,32 +1,3 @@
 module BacklogsHelper
 
-  class BacklogFieldsRows
-    include ActionView::Helpers::TagHelper
-
-    def initialize
-      @row = []
-    end
-
-    def add(*args)
-      args.any? ? @row << cells(*args) : @row
-    end
-
-    def to_html
-      @row.reduce(&:+)
-    end
-
-    def cells(label, text, options={})
-      options[:class] = [options[:class] || "", 'attribute'].join(' ')
-      content_tag 'div',
-        content_tag('div', label + ":", :class => 'label') + content_tag('div', text, :class => 'value'),
-        options
-    end
-  end
-
-  def backlog_fields_rows
-    r = BacklogFieldsRows.new
-    yield r
-    r.to_html
-  end
-
 end
