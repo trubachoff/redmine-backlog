df7e82825a850dd4af0a850b202a44fb2049389f
Vladimir Trubachoff <trubachoff@gmail.com>
2016-11-15 11:04:06 +0300

    Fix page bug and header

--- app/helpers/backlogs_helper.rb
+++ app/helpers/backlogs_helper.rb
@@ -1,21 +1,15 @@
 module BacklogsHelper
-  # include IssuesHelper
-
-  def queries_links(title, queries)
-    url_params = controller_name == 'issues' ? {:controller => 'issues', :action => 'index', :project_id => @project} : params
-    content_tag('div',
-      queries.collect {|query|
-          css = 'btn btn-default'
-          css << ' active' if query == @query
-          link_to(query.name, url_params.merge(:query_id => query), :class => css)
-        }.join("\n").html_safe,
-      :class => 'queries-buttons btn-group') + "\n"
-  end
 
   def render_queries_buttons
-    out = ''.html_safe
-    out << queries_links(l(:label_query_plural), sidebar_queries.reject(&:is_private?))
-    out
+    url_params = params
+    content_tag('div',
+      sidebar_queries.reject(&:is_private?)
+                     .collect {|query|
+                       css = 'btn btn-default'
+                       css << ' active' if query == @query
+                       link_to(query.name, url_params.merge(:query_id => query, :page => 1), :class => css)
+                     }.join("\n").html_safe,
+      :class => 'queries-buttons btn-group') + "\n"
   end
 
   def render_versions_buttons
